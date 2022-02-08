87ad64eb8a82081d009a3df5b810e188f8b54460
Louis-Rouh <louisrouhalde@gmail.com>
2022-02-08 12:05:23 +0100

    db create update

--- includes/db-create.php
+++ includes/db-create.php
@@ -1,7 +1,7 @@
 <?php
 /*Inclusion d'un fichier de configuration pour centraliser les informations de connexion*/
 
-function db_create($titre, $artiste, $album, $url, $date){
+function db_create($titre, $artiste, $album, $url, $id){
     require_once 'db-config.php';
 
     try {
@@ -11,14 +11,29 @@ function db_create($titre, $artiste, $album, $url, $date){
         die("<br>Erreur de connexion sur $dbname :" . $pe->getMessage());
     }
     
+    $create_req = "
+            CREATE TABLE IF NOT EXISTS tasks1 (
+                input_id    INTEGER PRIMARY KEY AUTOINCREMENT ,
+                titre       VARCHAR (255)        DEFAULT NULL,
+                artiste     VARCHAR (255)        DEFAULT NULL,
+                album       VARCHAR (255)        DEFAULT NULL,
+                url         VARCHAR (400)        DEFAULT NULL,
+                id          VARCHAR (255)        DEFAULT NULL,
+                date_ajout        DATE                 DEFAULT NULL
+                );
+    "
+    ;
+
     $insert_req = '
-            INSERT INTO tablename (titre, artiste, album, url, date)'
-            .'VALUES ("'.$titre.'", "'.$artiste.'", "'.$album.'", "'.$url.'", "'.$date.'");'
-            ;
-    
+            INSERT INTO tablename (titre, artiste, album, url, id, date_ajout)'
+            .'VALUES ("'.$titre.'", "'.$artiste.'", "'.$album.'", "'.$url.'", "'.$id.'", NOW());'
+    ;
+
+    $conn->exec($create_req);
     $conn->exec($insert_req);
     
     //Purger la requete SQL
+    $create_req = null;
     $insert_req = null;
     $conn = null;
 }
